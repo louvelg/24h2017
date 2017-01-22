3de6ce1d14e1917f936df3e9722edcd0cf9df183
Augustin Delale <adelale@hotmail.com>
2017-01-22 01:32:28 +0100

    bot2

--- src/main/java/com/akelio/codingame/client/bots/Bot2.java
+++ src/main/java/com/akelio/codingame/client/bots/Bot2.java
@@ -5,6 +5,9 @@ import java.util.Map;
 public class Bot2 implements Bot {
 
 	public DIR computeDirection(Map data) {
+		Map turn = (Map) data.get("lastTurn");
+		String s = (String) turn.get("data");
+		String me = (String) data.get("currentBot");
 		
 		return DIR.I;
 	}
